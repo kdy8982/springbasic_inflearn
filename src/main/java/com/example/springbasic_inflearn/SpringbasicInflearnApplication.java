679c0f8b24a9b904c06def955a1aaca8b04ef25e
dyKim <kdy8982@naver.com>
2023-01-03 22:00:16 +0900

    FIRST COMMIT

---
+++ src/main/java/com/example/springbasic_inflearn/SpringbasicInflearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbasic_inflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbasicInflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbasicInflearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbasic_inflearn/SpringbasicInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbasic_inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbasicInflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
