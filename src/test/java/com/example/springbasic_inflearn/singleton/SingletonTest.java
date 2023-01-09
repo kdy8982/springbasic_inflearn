f67c77ce773846c699983e658aed2ab5db76f513
dyKim <kdy8982@naver.com>
2023-01-09 17:30:17 +0900

    스프링 Singleton 검증 테스트 코드.

--- src/test/java/com/example/springbasic_inflearn/singleton/SingletonTest.java
+++ src/test/java/com/example/springbasic_inflearn/singleton/SingletonTest.java
@@ -5,6 +5,7 @@ import com.example.springbasic_inflearn.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,4 +34,14 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
     }
 
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    public void springContainer() throws Exception {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        assertThat(memberService1).isSameAs(memberService2);
+    }
 }
